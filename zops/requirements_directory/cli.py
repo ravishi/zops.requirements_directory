6587bffd2e0de46b7f363aec33ccb4e70bfbb546
Alexandre Andrade <kaniabi@gmail.com>
2018-04-27 08:53:00 -0300

    Upgrading to support piptools 2.X.

--- zops/requirements_directory/cli.py
+++ zops/requirements_directory/cli.py
@@ -73,14 +73,15 @@ def compile(update):
 @main.command()
 @click.option('--rebuild', is_flag=True, help='Clear any caches upfront, rebuild from scratch.')
 @click.argument('directories', nargs=-1)
-def upgrade(rebuild, directories):
+@click.pass_context
+def upgrade(click_context, rebuild, directories):
     """
     New algorithm to upgrade requirements directory.
     """
     from glob import glob
 
     # NOTE: Create before the loop to maintain the dependencies-cache.
-    pip_tools = PipTools()
+    pip_tools = PipTools(click_context)
 
     directories = directories or ['.']
     for i_directory in directories:
@@ -172,21 +173,23 @@ def fixes(filename, temporary_dependencies):
 
 class PipTools(object):
 
-    def __init__(self):
-        from zops.piptools.cache import DependencyCache
+    def __init__(self, click_context):
+        from piptools.cache import DependencyCache
         self.dependency_cache = DependencyCache()
 
         # NOTE: Debugging
         # Show some debugging information.
-        from zops.piptools.logging import log
+        from piptools.logging import log
         log.verbose = True
+
+        self.__click_context = click_context
 
     def compile(self, dst_file, src_files, upgrade=False, rebuild=False, new_code=False):
         """
         Performs pip-compile (from piptools) with a twist.
 
         We force editable requirements to use GIT repository (parameter obtain=True) so we have setuptools_scm working on
-        them (we use setuptools_scm).
+        them (we use setuptools_scm). Not sure this is working with piptools 2.X.
         """
         from pip.req.req_install import InstallRequirement
 
@@ -196,199 +199,17 @@ class PipTools(object):
             InstallRequirement.update_editable_ = InstallRequirement.update_editable
             InstallRequirement.update_editable = lambda s, _o: InstallRequirement.update_editable_(s, True)
 
-        return self._compile(dst_file, src_files, upgrade=upgrade, rebuild=rebuild)
-
-    def _compile(
-        self,
-        dst_file,
-        src_files,
-        upgrade=False,
-        upgrade_packages=[],
-        rebuild=False,
-        allow_unsafe=False,
-        max_rounds=10,
-        generate_hashes=False,
-        annotate=True,
-        header=True,
-        index=False,
-        emit_trusted_host=False,
-        dry_run=False,
-    ):
-        from zops.piptools.scripts.compile import get_pip_command
-        from zops.piptools.repositories import PyPIRepository, LocalRequirementsRepository
-        from zops.piptools.utils import key_from_req, is_pinned_requirement, dedup
-        from zops.piptools.resolver import Resolver
-        from zops.piptools.logging import log
-        from pip.req import InstallRequirement, parse_requirements
-        import tempfile
-        import sys
-
-        pip_command = get_pip_command()
-
-        pip_args = []
-        # if find_links:
-        #     for link in find_links:
-        #         pip_args.extend(['-f', link])
-        # if index_url:
-        #     pip_args.extend(['-i', index_url])
-        # if extra_index_url:
-        #     for extra_index in extra_index_url:
-        #         pip_args.extend(['--extra-index-url', extra_index])
-        # if cert:
-        #     pip_args.extend(['--cert', cert])
-        # if client_cert:
-        #     pip_args.extend(['--client-cert', client_cert])
-        # if pre:
-        #     pip_args.extend(['--pre'])
-        # if trusted_host:
-        #     for host in trusted_host:
-        #         pip_args.extend(['--trusted-host', host])
-
-        pip_options, _ = pip_command.parse_args(pip_args)
-
-        session = pip_command._build_session(pip_options)
-        repository = PyPIRepository(pip_options, session)
-
-        # Proxy with a LocalRequirementsRepository if --upgrade is not specified
-        # (= default invocation)
-        if not upgrade and os.path.exists(dst_file):
-            ireqs = parse_requirements(
-                dst_file, finder=repository.finder, session=repository.session, options=pip_options
-            )
-            # Exclude packages from --upgrade-package/-P from the existing pins: We want to upgrade.
-            upgrade_pkgs_key = {
-                key_from_req(InstallRequirement.from_line(pkg).req)
-                for pkg in upgrade_packages
-            }
-            existing_pins = {
-                key_from_req(ireq.req): ireq
-                for ireq in ireqs
-                if is_pinned_requirement(ireq) and key_from_req(ireq.req) not in upgrade_pkgs_key
-            }
-            repository = LocalRequirementsRepository(existing_pins, repository)
-
-        log.debug('Using indexes:')
-        # remove duplicate index urls before processing
-        repository.finder.index_urls = list(dedup(repository.finder.index_urls))
-        for index_url in repository.finder.index_urls:
-            log.debug('  {}'.format(index_url))
-
-        if repository.finder.find_links:
-            log.debug('')
-            log.debug('Configuration:')
-            for find_link in repository.finder.find_links:
-                log.debug('  -f {}'.format(find_link))
-
-        ###
-        # Parsing/collecting initial requirements
-        ###
-
-        constraints = []
-        for src_file in src_files:
-            is_setup_file = os.path.basename(src_file) == 'setup.py'
-            if is_setup_file or src_file == '-':
-                # pip requires filenames and not files. Since we want to support
-                # piping from stdin, we need to briefly save the input from stdin
-                # to a temporary file and have pip read that.  also used for
-                # reading requirements from install_requires in setup.py.
-                tmpfile = tempfile.NamedTemporaryFile(mode='wt', delete=False)
-                if is_setup_file:
-                    from distutils.core import run_setup
-                    dist = run_setup(src_file)
-                    tmpfile.write('\n'.join(dist.install_requires))
-                else:
-                    tmpfile.write(sys.stdin.read())
-                tmpfile.flush()
-                constraints.extend(parse_requirements(
-                    tmpfile.name, finder=repository.finder, session=repository.session, options=pip_options))
-            else:
-                constraints.extend(parse_requirements(
-                    src_file, finder=repository.finder, session=repository.session, options=pip_options))
-
-        # Check the given base set of constraints first
-        Resolver.check_constraints(constraints)
-
-        from zops.piptools.exceptions import PipToolsError
-        try:
-            resolver = Resolver(
-                constraints,
-                repository,
-                prereleases=None,
-                cache=self.dependency_cache,
-                clear_caches=rebuild,
-                allow_unsafe=allow_unsafe
-            )
-            results = resolver.resolve(max_rounds=max_rounds)
-            if generate_hashes:
-                hashes = resolver.resolve_hashes(results)
-            else:
-                hashes = None
-        except PipToolsError as e:
-            log.error(str(e))
-            sys.exit(2)
-
-        log.debug('')
-
-        ##
-        # Output
-        ##
-
-        # Compute reverse dependency annotations statically, from the
-        # dependency cache that the resolver has populated by now.
-        #
-        # TODO (1a): reverse deps for any editable package are lost
-        #            what SHOULD happen is that they are cached in memory, just
-        #            not persisted to disk!
-        #
-        # TODO (1b): perhaps it's easiest if the dependency cache has an API
-        #            that could take InstallRequirements directly, like:
-        #
-        #                cache.set(ireq, ...)
-        #
-        #            then, when ireq is editable, it would store in
-        #
-        #              editables[egg_name][link_without_fragment] = deps
-        #              editables['pip-tools']['git+...ols.git@future'] = {'click>=3.0', 'six'}
-        #
-        #            otherwise:
-        #
-        #              self[as_name_version_tuple(ireq)] = {'click>=3.0', 'six'}
-        #
-        reverse_dependencies = None
-        if annotate:
-            reverse_dependencies = resolver.reverse_dependencies(results)
-
-        from zops.piptools.writer import OutputWriter
-        writer = OutputWriter(
-            src_files,
-            dst_file,
-            dry_run=dry_run,
-            emit_header=header,
-            emit_index=index,
-            emit_trusted_host=emit_trusted_host,
-            annotate=annotate,
-            generate_hashes=generate_hashes,
-            default_index_url=repository.DEFAULT_INDEX_URL,
-            index_urls=repository.finder.index_urls,
-            trusted_hosts=pip_options.trusted_hosts,
-            format_control=repository.finder.format_control
+        from piptools.scripts.compile import cli
+        return self.__click_context.invoke(
+            cli,
+            output_file=dst_file,
+            src_files=src_files,
+            upgrade=upgrade,
+            rebuild=rebuild,
+            emit_trusted_host=False,
+            header=False,
+            index=False,
         )
-        writer.write(
-            results=results,
-            unsafe_requirements=resolver.unsafe_constraints,
-            reverse_dependencies=reverse_dependencies,
-            primary_packages={key_from_req(ireq.req) for ireq in constraints if not ireq.constraint},
-            markers={
-                key_from_req(ireq.req): ireq.markers
-                for ireq in constraints
-                if ireq.markers
-            },
-            hashes=hashes,
-            allow_unsafe=allow_unsafe
-        )
-
-        if dry_run:
-            log.warning('Dry-run, so nothing updated.')
 
 
 if __name__ == '__main__':
